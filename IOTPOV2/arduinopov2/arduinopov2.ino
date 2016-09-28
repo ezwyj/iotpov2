3dd0d55b4b54d8689868ea56a66d308c434d4655
2480446748 <2480446748@qq.com>
2016-09-28 22:21:00 +0800

    同步

---
+++ IOTPOV2/arduinopov2/arduinopov2.ino
@@ -0,0 +1,25 @@
+#include <Adafruit_DotStar.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <SPI.h> 
+
+#define MaxStripNum 20
+#define switchPin 2 
+
+const char* ssid = "ChinaNetuPTU";
+const char* password = "h5rcuvdw";
+const char* mqtt_server = "120.76.147.152";
+
+void setup()
+{
+
+  /* add setup code here */
+
+}
+
+void loop()
+{
+
+  /* add main program code here */
+
+}
