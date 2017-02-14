af7c2b677f59abb1791baaa14aae69f5d52b4097
2480446748@qq.com <2480446748@qq.com>
2017-02-14 18:01:22 +0800

    20170214

--- IOTPOV2/Web/Controllers/DemoController.cs
+++ IOTPOV2/Web/Controllers/DemoController.cs
@@ -2,6 +2,8 @@
 using Common.Model;
 using Common.Util;
 using Senparc.Weixin;
+using Senparc.Weixin.Exceptions;
+using Senparc.Weixin.MP;
 using Senparc.Weixin.MP.AdvancedAPIs;
 using Senparc.Weixin.MP.AdvancedAPIs.OAuth;
 using Senparc.Weixin.MP.AdvancedAPIs.QrCode;
@@ -35,18 +37,21 @@ namespace Web.Controllers
             var device = PovDevice.GetList();
             foreach (var deviceItem in device)
             {
-                povDevices.Add(deviceItem.DeviceName, deviceItem);
+                if (!povDevices.ContainsKey(deviceItem.DeviceName))
+                {
+                    povDevices.Add(deviceItem.DeviceName, deviceItem);
+                }
             }
         }
         private OAuthAccessTokenResult GetOAuthAccessTokenResult(string code, string state, out string msg)
         {
-            if (string.IsNullOrEmpty(code))
-            {
-                msg = "no Code";
-                return null;
-            }
+            //if (string.IsNullOrEmpty(code))
+            //{
+            //    msg = "no Code";
+            //    return null;
+            //}
 
-            if (state != "JeffreySu")
+            if (state != "Device001")
             {
                 //这里的state其实是会暴露给客户端的，验证能力很弱，这里只是演示一下
                 //实际上可以存任何想传递的数据，比如用户ID，并且需要结合例如下面的Session["OAuthAccessToken"]进行验证
@@ -78,21 +83,60 @@ namespace Web.Controllers
             msg = "OK";
             return result;
         }
-
-        public ActionResult Index()
+        /// <summary>
+        /// 介绍使用流程页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index(string deviceName)
         {
+            ViewData["UrlBase"] = OAuthApi.GetAuthorizeUrl(appId, "http://demo1.deviceiot.top/iot/Demo/Create", deviceName, OAuthScope.snsapi_base);
             return View();
         }
 
        
 
-        public ActionResult Create(string deviceName,string code,string state)
+        public ActionResult Create(string code,string state)
         {
             string msg = string.Empty;
-            OAuthAccessTokenResult result = GetOAuthAccessTokenResult(code, state, out msg);
-            OAuthUserInfo userInfo = OAuthApi.GetUserInfo(result.access_token, result.openid);
-            ViewBag.JsSdkPackage = JSSDKHelper.GetJsSdkUiPackage(appId, secret, Request.Url.AbsoluteUri);
-            return View();
+            if (string.IsNullOrEmpty(code))
+            {
+                return Content("您拒绝了授权！");
+            }
+
+            if (state != "Device001")
+            {
+                //这里的state其实是会暴露给客户端的，验证能力很弱，这里只是演示一下
+                //实际上可以存任何想传递的数据，比如用户ID，并且需要结合例如下面的Session["OAuthAccessToken"]进行验证
+                return Content("验证失败！请从正规途径进入！");
+            }
+
+            //通过，用code换取access_token
+            var result = OAuthApi.GetAccessToken(appId, secret, code);
+            if (result.errcode != ReturnCode.请求成功)
+            {
+                return Content("错误：" + result.errmsg);
+            }
+
+            //下面2个数据也可以自己封装成一个类，储存在数据库中（建议结合缓存）
+            //如果可以确保安全，可以将access_token存入用户的cookie中，每一个人的access_token是不一样的
+            Session["OAuthAccessTokenStartTime"] = DateTime.Now;
+            Session["OAuthAccessToken"] = result;
+
+            //因为这里还不确定用户是否关注本微信，所以只能试探性地获取一下
+            OAuthUserInfo userInfo = null;
+            try
+            {
+                //已关注，可以得到详细信息
+                userInfo = OAuthApi.GetUserInfo(result.access_token, result.openid);
+ 
+                return View("UserInfoCallback", userInfo);
+            }
+            catch (ErrorJsonResultException ex)
+            {
+                //未关注，只能授权，无法得到详细信息
+                //这里的 ex.JsonResult 可能为："{\"errcode\":40003,\"errmsg\":\"invalid openid\"}"
+                return Content("用户已授权，授权Token：" + result);
+            }
         }
 
         public ActionResult Pay()
